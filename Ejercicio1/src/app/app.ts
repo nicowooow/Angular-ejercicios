b8aee2c4a79020774024bef0a2c78a22c659b239
nicowooow <nicolasgianfranco123@gmail.com>
2026-05-07 13:49:11 +0200

    se crea la lista de tareas con su id, titulo y si esta completada o no

--- Ejercicio1/src/app/app.ts
+++ Ejercicio1/src/app/app.ts
@@ -1,13 +1,18 @@
 import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Tareas} from './tareas/tareas';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Tareas],
+  imports: [RouterOutlet,Tareas,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Ejercicio1');
+  // protected readonly title = signal('Ejercicio1');
+  protected tituloTarea = "";
+  CrearTarea(){
+
+  }
 }
--- Ejercicio1/src/app/tareas/tareas.ts
+++ Ejercicio1/src/app/tareas/tareas.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+
+interface ITarea {
+  id: number,
+  titulo: string,
+  completada: boolean,
+}
 
 @Component({
   selector: 'app-tareas',
@@ -7,5 +13,32 @@ import { Component } from '@angular/core';
   styleUrl: './tareas.css',
 })
 export class Tareas {
+  protected readonly tareas = signal<ITarea[]>([
+    {
+      "id": 1,
+      "titulo": "Comprar comida",
+      "completada": false,
+    },
+    {
+      "id": 2,
+      "titulo": "Terminar proyecto Angular",
+      "completada": false,
+    },
+    {
+      "id": 3,
+      "titulo": "Pagar cuentas",
+      "completada": true,
+    },
+    {
+      "id": 4,
+      "titulo": "Ir al gimnasio",
+      "completada": false,
+    },
+    {
+      "id": 5,
+      "titulo": "Estudiar para examen de Derecho",
+      "completada": false,
+    }
+  ]);
 
 }
