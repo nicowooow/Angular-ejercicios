41c039eea219b0e8ff028643fb927de8e9c39a9c
nicowooow <nicolasgianfranco123@gmail.com>
2026-05-14 13:33:50 +0200

    crear el componente de boton

---
+++ Ejercicio1/src/app/button/button.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.css',
+})
+export class Button {
+
+
+}
